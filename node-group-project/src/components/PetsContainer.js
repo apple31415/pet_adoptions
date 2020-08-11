25c2d4dc3e3c4d76384f3d226853891a5fa7e2e7
John Bobst <johnabobst@gmail.com>
2020-08-11 11:44:54 -0700

    PetsContainer
    Added component for displaying pet types

--- node-group-project/src/components/PetsContainer.js
+++ node-group-project/src/components/PetsContainer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const PetsContainer = (props) => {
-  return (
+  const [petTypes, setPetTypes] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/pets')
+      .then(result => setPetTypes(result.rows))
+  }, []);
+  let mappedPetTypes = petTypes.map((petType) => {
     <div>
+      <Link to={`/pets/${petType.type}`} >{petType.type}</Link>
+      <p>{petType.description}</p>
     </div>
+  });
+  return (
+    <>
+      {mappedPetTypes}
+    </>
   );
 }
 
