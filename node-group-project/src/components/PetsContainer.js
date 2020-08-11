b96740b3cb6f5c6b29cab1cec28c508557e1998c
Ashley Kane <kane.ashleymay@gmail.com>
2020-08-11 16:52:51 -0400

    API endpoint for PetsContainer
    Plus some minor bugs from not having an endpoint. :)

--- node-group-project/src/components/PetsContainer.js
+++ node-group-project/src/components/PetsContainer.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import _ from "lodash"
+import { Link } from "react-router-dom"
 
 const PetsContainer = (props) => {
   const [petTypes, setPetTypes] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/pets')
-      .then(result => setPetTypes(result.rows))
+    fetch('http://localhost:3000/api/v1/pettypes')
+    .then(result => result.json())
+    .then(petTypes => {
+      setPetTypes(petTypes)
+    })
   }, []);
 
-  let mappedPetTypes = petTypes.map((petType) => {
-    <div>
-      <Link to={`/pets/${petType.type}`} >{petType.type}</Link>
-      <p>{petType.description}</p>
-    </div>
-  });
-  
+  let mappedPetTypes
+  if(petTypes) {
+    mappedPetTypes = petTypes.map((petType, index) => {
+     return(
+      <div className="pet-type-item" key={index}>
+        <Link to={`/pets/${petType.type}`} >{petType.type}</Link>
+        <p>{petType.description}</p>
+      </div>
+     )
+    });
+  }
+
   return (
-    <>
+    <div className="pet-type-container">
+      <h1>Pet Types</h1>
       {mappedPetTypes}
-    </>
+    </div>
   );
 }
 
