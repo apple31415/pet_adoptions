d990e41f3b8e264a217d96b201c9cf8069cbb420
John Bobst <johnabobst@gmail.com>
2020-08-11 12:30:46 -0700

    Added pet type container

--- node-group-project/src/components/PetTypeContainer.js
+++ node-group-project/src/components/PetTypeContainer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const PetTypeContainer = (props) => {
-  return (
+  const [adoptablePets, setAdoptablePets] = useState([]);
+
+  let petType = props.match.params.petType;
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/pets/${petType}`)
+      .then(result => setAdoptablePets(result.rows))
+      .catch(error => console.log(error))
+  }, []);
+
+  let adoptablePetElements = adoptablePets.map(pet => {
     <div>
+      <Link to={`/pets/${petType}/${pet.id}`}>
+        <h4>{pet.name}</h4>
+      </Link>
+      <img src={`${pet.image_url}`} aria-label="image of an adoptable pet"/>
     </div>
+  })
+
+  return (
+    <>
+      {adoptablePetElements}
+    </>
   );
 }
 
