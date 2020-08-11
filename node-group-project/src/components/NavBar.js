6473f88638431cfd5f5656b601d7185dbe895efc
Ashley Kane <kane.ashleymay@gmail.com>
2020-08-11 14:30:14 -0400

    NavBar

--- node-group-project/src/components/NavBar.js
+++ node-group-project/src/components/NavBar.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from 'react'
+import PetsContainer from "./PetsContainer"
+import PetTypeContainer from "./PetTypeContainer"
+import PetShow from './PetShow'
 
 const NavBar = (props) => {
   return (
     <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pets" component={PetsContainer} />
+          <Route exact path="/pets/:petType" component={PetTypeContainer} />
+          <Route exact path="/pets/pet_type/:id" component={PetShow} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default NavBar;
